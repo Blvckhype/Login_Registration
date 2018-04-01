d2502a45e7bef882be5a6ecc3b4480349aed6c06
Blvck <mati12899@o2.pl>
2018-04-01 17:22:23 +0200

    Added Google signIn Option

--- app/src/main/java/com/example/mati/login_registration/MainActivity.java
+++ app/src/main/java/com/example/mati/login_registration/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.mati.login_registration;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String name = getIntent().getStringExtra("Name");
+        String surname = getIntent().getStringExtra("Surname");
+
+        TextView myText = findViewById(R.id.mainText);
+        myText.setText(name + surname);
+
     }
 }
